b10049a56b8165d3dedc21399d66b6e3d625dbfc
Ehm27 <erick@coolhumans.team>
2022-07-11 20:57:34 -0500

    contact page creation

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,29 +1,29 @@
 import Head from "next/head";
 import Link from "next/link";
-import { NavBar } from "../components/NavBar";
-import styles from "../styles/Home.module.css";
+import { NavBar } from "../../components/NavBar";
+import styles from "../../styles/Home.module.css";
 
-export default function AboutPage() {
+export default function ContactPage() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>About - Erick Hernández</title>
-        <meta name="description" content="About Page" />
+        <title>Contact - Erick Hernández</title>
+        <meta name="description" content="Contact Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <NavBar />
 
       <main className={styles.main}>
-        <h1>About Page</h1>
+        <h1>Contact Page</h1>
         <h1 className={styles.title}>
           {/* Ir a <a href="/">Home</a> */}
           Ir a <Link href="/">Home</Link>
         </h1>
 
         <p className={styles.description}>
-          ESTAMOS EN ABOUT
-          <code className={styles.code}>pages/about.js</code>
+          ESTAMOS EN CONTACT
+          <code className={styles.code}>pages/contact.js</code>
         </p>
       </main>
     </div>
